fe619b670d25dc490b10af4c28dd9663c2ed6c94
BicoSteve101 <steve.bico@giktek.io>
2025-11-29 20:51:37 +0300

    first commit

--- zula-validation-library/src/main/java/com/zula/validation/config/ValidationAutoConfig.java
+++ zula-validation-library/src/main/java/com/zula/validation/config/ValidationAutoConfig.java
@@ -1,19 +1,5 @@
 package com.zula.validation.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "zula.validation")
-public class ValidationProperties {
-    private boolean enabled = true;
-    private boolean failFast = true;
-
-    public boolean isEnabled() { return enabled; }
-    public void setEnabled(boolean enabled) { this.enabled = enabled; }
-
-    public boolean isFailFast() { return failFast; }
-    public void setFailFast(boolean failFast) { this.failFast = failFast; }
-}package com.zula.validation.config;
-
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
